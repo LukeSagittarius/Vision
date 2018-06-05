325b00811ba5499ae083cf54e1b0101eb0e8e463
Łukasz Machaj <lukasz.jan.machaj@gmail.com>
2018-06-05 18:59:12 +0200

    Database JUnits

--- src/test/java/com/smarthome/server/DatabaseTest.java
+++ src/test/java/com/smarthome/server/DatabaseTest.java
@@ -1,27 +1,42 @@
 package com.smarthome.server;
 
+import com.smarthome.server.dto.MinisterialEffectDto;
+import com.smarthome.server.entity.Area;
+import com.smarthome.server.entity.MinisterialEffect;
+import com.smarthome.server.entity.Profile;
 import com.smarthome.server.facade.MinisterialEffectMaintenanceService;
+import com.smarthome.server.mapper.MinisterialEffectMapper;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mapstruct.factory.Mappers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import java.io.IOException;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
 
 @RunWith(SpringRunner.class)
 @SpringBootTest()
 public class DatabaseTest {
 
     @Autowired
-    MinisterialEffectMaintenanceService facade;
+    MinisterialEffectMaintenanceService ministerialEffectMaintenanceService;
 
     @Test
-    public void shouldPersistAndRetrieveDevices() throws IOException {
-//        DevicesDto expected = new Application().getDevices();
-//        facade.save(DevicesAssembler.dtoToEntity(expected));
-//        Devices entity = facade.findById(1L);
-//        DevicesDto result = DevicesAssembler.entityToDto(entity);
-//        Assert.assertTrue(result.equals(expected));
+    public void shouldPersistAndRetrieveDevices() {
+        MinisterialEffectDto expected = createMinisterialEffectDto();
+        MinisterialEffect entityToSave = Mappers.getMapper(MinisterialEffectMapper.class).ministerialEffectDtoToMinisterialEffect(expected);
+        ministerialEffectMaintenanceService.save(entityToSave);
+        MinisterialEffect foundEntity = ministerialEffectMaintenanceService.findById(1L);
+        MinisterialEffectDto result = Mappers.getMapper(MinisterialEffectMapper.class).ministerialEffectToMinisterialEffectDto(foundEntity);
+        assertThat(expected.getCode(), is(result.getCode()));
+    }
+
+    private MinisterialEffectDto createMinisterialEffectDto() {
+        MinisterialEffectDto dto = new MinisterialEffectDto();
+        dto.setCode("CODE");
+        return dto;
     }
 }
